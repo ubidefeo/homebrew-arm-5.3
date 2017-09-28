8a708f8e8504e6bb70353d4ecdc85104f287ab7d
ubi de feo <me@ubidefeo.com>
2017-09-28 14:18:41 +0200

    - fixed sha256 signature [ forgot -a 256 during generation ]

--- arm-gcc-bin-53.rb
+++ arm-gcc-bin-53.rb
@@ -5,7 +5,7 @@ class ArmGccBin53 < Formula
 
     homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm'
     url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/5_3-2016q1/gccarmnoneeabi532016q120160330mactar.bz2'
-    sha256 '3f62a6cdaef06145aedafe0f6da5fb42a077f35e'
+    sha256 '480843ca1ce2d3602307f760872580e999acea0e34ec3d6f8b6ad02d3db409cf'
 
     version '5-2016-q1-update'
 
